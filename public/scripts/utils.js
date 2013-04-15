8470a4efb3d7062901dda160044db1410c5fdccc
mike.zhu <mikezhuyuan@hotmail.com>
2013-04-15 22:10:49 +0800

    Auto selection all

---
+++ public/scripts/utils.js
@@ -0,0 +1,9 @@
+window.Utils = {
+    selectContents: function(el){
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+}
